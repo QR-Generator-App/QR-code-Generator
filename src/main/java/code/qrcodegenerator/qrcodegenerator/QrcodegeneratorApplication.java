2022c88a89cb268d0ecda37946b1cca6273a7aec
Sreekanth V <sreekanthv1995@gmail.com>
2024-07-12 10:05:19 +0530

    first commit

---
+++ src/main/java/code/qrcodegenerator/qrcodegenerator/QrcodegeneratorApplication.java
@@ -0,0 +1,13 @@
+package code.qrcodegenerator.qrcodegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcodegeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrcodegeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/code/qrcodegenerator/qrcodegenerator/QrcodegeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package code.qrcodegenerator.qrcodegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodegeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
